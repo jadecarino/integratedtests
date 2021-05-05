0fd77e45bd1878a6c77b401d1d418963aeeabc24
Michael Baylis <Michael.Baylis@uk.ibm.com>
2021-05-05 11:21:57 +0100

    Add java local int tests
    Signed-off-by: Michael Baylis <Michael.Baylis@uk.ibm.com>

---
+++ galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/core/AbstractCoreLocal.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed Materials - Property of IBM
+ * 
+ * (c) Copyright IBM Corp. 2021.
+ */
+package dev.galasa.inttests.core;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.gson.JsonObject;
+
+import dev.galasa.Test;
+import dev.galasa.galasaecosystem.IGenericEcosystem;
+
+public abstract class AbstractCoreLocal {
+    
+    @Test
+    public void testCoreIvtTest() throws Exception {
+        
+        String runName = getEcosystem().submitRun(null, 
+                null, 
+                null, 
+                "dev.galasa.core.manager.ivt", 
+                "dev.galasa.core.manager.ivt.CoreManagerIVT", 
+                null, 
+                null, 
+                null, 
+                null);
+        
+        JsonObject run = getEcosystem().waitForRun(runName);
+        
+        String result = run.get("result").getAsString();
+        
+        assertThat(result).as("The test indicates the test passes").isEqualTo("Passed");
+    }
+
+    abstract protected IGenericEcosystem getEcosystem();
+
+}
