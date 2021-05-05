67311010ac6e2ca2489f3af7d8fef8da4a638ee7
Michael Baylis <Michael.Baylis@uk.ibm.com>
2021-05-05 18:49:57 +0100

    Add Artifact Manager local tests
    Signed-off-by: Michael Baylis <Michael.Baylis@uk.ibm.com>

---
+++ galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/artifact/AbstractArtifactLocal.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed Materials - Property of IBM
+ * 
+ * (c) Copyright IBM Corp. 2021.
+ */
+package dev.galasa.inttests.artifact;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.gson.JsonObject;
+
+import dev.galasa.Test;
+import dev.galasa.galasaecosystem.IGenericEcosystem;
+
+public abstract class AbstractArtifactLocal {
+    
+    @Test
+    public void testCoreIvtTest() throws Exception {
+        
+        String runName = getEcosystem().submitRun(null, 
+                null, 
+                null, 
+                "dev.galasa.artifact.manager.ivt", 
+                "dev.galasa.artifact.manager.ivt.ArtifactManagerIVT", 
+                null, 
+                null, 
+                null, 
+                null);
+        
+        JsonObject run = getEcosystem().waitForRun(runName);
+        
+        String result = run.get("result").getAsString();
+        
+        assertThat(result).as("The test indicates the test passes").isEqualTo("Passed");
+    }
+
+    abstract protected IGenericEcosystem getEcosystem();
+
+}
