da5267002c659b5de3cd62a84025ea7b6e2f9198
Will Yates <hobbit1983@gmail.com>
2021-05-25 00:18:58 +0100

    Integration Test for zosBatchManager
    Signed-off-by: Will Yates <hobbit1983@gmail.com>

---
+++ galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/zosBatch/AbstractZosBatchLocal.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed Materials - Property of IBM
+ * 
+ * (c) Copyright IBM Corp. 2021.
+ */
+package dev.galasa.inttests.zosBatch;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.gson.JsonObject;
+
+import dev.galasa.Test;
+import dev.galasa.galasaecosystem.IGenericEcosystem;
+
+public abstract class AbstractZosBatchLocal {
+    
+    @Test
+    public void testCoreIvtTest() throws Exception {
+        
+        String runName = getEcosystem().submitRun(null, 
+                null, 
+                null, 
+                "dev.galasa.zos.manager.ivt", 
+                "dev.galasa.zos.manager.ivt.ZosManagerBatchIVT", 
+                null, 
+                null, 
+                null, 
+                null);
+        
+        JsonObject run = getEcosystem().waitForRun(runName);
+        
+        String result = run.get("result").getAsString();
+        
+        assertThat(result).as("The test indicates the test passes").isEqualTo("Passed");
+    }
+
+    abstract protected IGenericEcosystem getEcosystem();
+
+}
