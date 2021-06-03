51e413b3e39a4cfeeec8822a5b000e4582522f79
Will Yates <hobbit1983@gmail.com>
2021-06-03 15:29:45 +0100

    attempt to use RSE and z/OSMF
    Signed-off-by: Will Yates <hobbit1983@gmail.com>

--- galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/zosBatch/AbstractZosBatchLocal.java
+++ galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/zosBatch/AbstractZosBatchLocal.java
@@ -15,7 +15,33 @@ import dev.galasa.galasaecosystem.IGenericEcosystem;
 public abstract class AbstractZosBatchLocal {
     
     @Test
-    public void testZosBatchIvtTest() throws Exception {
+    public void testZosBatchZOSMFTest() throws Exception {
+    	
+    	//default to z/OSMF batch
+    	getEcosystem().setCpsProperty("zos.bundle.extra.batch.manager", "dev.galasa.zosbatch.zosmf.manager");
+        
+        String runName = getEcosystem().submitRun(null, 
+                null, 
+                null, 
+                "dev.galasa.zos.manager.ivt", 
+                "dev.galasa.zos.manager.ivt.ZosManagerBatchIVT", 
+                null, 
+                null, 
+                null, 
+                null);
+        
+        JsonObject run = getEcosystem().waitForRun(runName);
+        
+        String result = run.get("result").getAsString();
+        
+        assertThat(result).as("The test indicates the test passes").isEqualTo("Passed");
+    }
+    
+    @Test
+    public void testZosBatchRSEIvtTest() throws Exception {
+    	
+    	//Use RSE batch
+    	getEcosystem().setCpsProperty("zos.bundle.extra.batch.manager", "dev.galasa.zosbatch.rseapi.manager");
         
         String runName = getEcosystem().submitRun(null, 
                 null, 
