84eaf4f894499822354309d1d37634117f1f59e0
Matthew Chivers <matthewchivers@outlook.com>
2021-08-18 12:01:31 +0100

    Corrected capabilities (#96)
    * Corrected capabilities
    
    Signed-off-by: Matthew Chivers <matthewchivers@outlook.com>
    
    * Corrected copyright statements
    
    Signed-off-by: Matthew Chivers <matthewchivers@outlook.com>

--- galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/compilation/offline/mvp/CompilationLocalOfflineJava11UbuntuMVP.java
+++ galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/compilation/offline/mvp/CompilationLocalOfflineJava11UbuntuMVP.java
@@ -1,8 +1,6 @@
 /*
- * Licensed Materials - Property of IBM
- * 
- * (c) Copyright IBM Corp. 2021.
- */
+* Copyright contributors to the Galasa project 
+*/
 package dev.galasa.inttests.compilation.offline.mvp;
 
 import java.io.IOException;
@@ -29,7 +27,7 @@ public class CompilationLocalOfflineJava11UbuntuMVP extends AbstractCompilationL
     @LocalEcosystem(linuxImageTag = "PRIMARY", isolationInstallation = IsolationInstallation.Mvp)
     public ILocalEcosystem ecosystem;
     
-    @LinuxImage(operatingSystem = OperatingSystem.ubuntu, capabilities = "mvp")
+    @LinuxImage(operatingSystem = OperatingSystem.ubuntu, capabilities = "isolated")
     public ILinuxImage linuxImage;
     
     @JavaUbuntuInstallation(javaVersion = JavaVersion.v11)
