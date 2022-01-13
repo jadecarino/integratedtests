2b8ea338e3b0100f6f68a40359ed2b4389e4e258
Jade Carino <65225359+jadecarino@users.noreply.github.com>
2022-01-13 12:00:48 +0000

    Integration tests for the CICSTSManagerIVT (#117)
    * Add RSE tests back in
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Remove CECIK8S inttest
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>
    
    * Integration tests for the CICSTSManagerIVT
    
    Signed-off-by: Jade Carino <jade.carino@ibm.com>

---
+++ galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/cicsts/AbstractCICSTSLocal.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright contributors to the Galasa project
+ */
+
+package dev.galasa.inttests.cicsts;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.gson.JsonObject;
+
+import dev.galasa.Test;
+import dev.galasa.galasaecosystem.IGenericEcosystem;
+
+public abstract class AbstractCICSTSLocal {
+
+    @Test
+    public void testCICSTSIvtTest() throws Exception {
+
+        getEcosystem().setCpsProperty("cicsts.dse.tag.PRIMARY.applid", "IYK2ZNB5");
+        getEcosystem().setCpsProperty("cicsts.provision.type", "DSE");
+        getEcosystem().setCpsProperty("cicsts.default.logon.initial.text", "HIT ENTER FOR LATEST STATUS");
+        getEcosystem().setCpsProperty("cicsts.default.logon.gm.text", "******\\(R)");
+
+        String runName = getEcosystem().submitRun(null, 
+                null, 
+                null, 
+                "dev.galasa.cicsts.cicsts.manager.ivt",
+                "dev.galasa.cicsts.cicsts.manager.ivt.CICSTSManagerIVT", 
+                null, 
+                null, 
+                null, 
+                null);
+        
+        JsonObject run = getEcosystem().waitForRun(runName);
+        
+        String result = run.get("result").getAsString();
+        
+        assertThat(result).as("The test indicates the test passes").isEqualTo("Passed");
+
+    }
+
+    abstract protected IGenericEcosystem getEcosystem();
+    
+}
