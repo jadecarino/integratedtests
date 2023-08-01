e388d37ced0eadca09697339567216829741d213
Fiona Ampofo <64271621+Akyiaa@users.noreply.github.com>
2023-08-01 10:08:41 +0100

    applied copyright changes - note extra comment (#180)
    Signed-off-by: Fiona Ampofo <64271621+Akyiaa@users.noreply.github.com>

--- galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/compilation/offline/mvp/CompilationLocalOfflineJava11UbuntuMVP.java
+++ galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/compilation/offline/mvp/CompilationLocalOfflineJava11UbuntuMVP.java
@@ -1,6 +1,8 @@
 /*
-* Copyright contributors to the Galasa project 
-*/
+ * Copyright contributors to the Galasa project
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package dev.galasa.inttests.compilation.offline.mvp;
 
 import java.io.IOException;
--- galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/simbank/local/SimBankLocalJava11Ubuntu.java
+++ galasa-inttests-parent/dev.galasa.inttests/src/main/java/dev/galasa/inttests/simbank/local/SimBankLocalJava11Ubuntu.java
@@ -1,6 +1,8 @@
 /*
-* Copyright contributors to the Galasa project 
-*/
+ * Copyright contributors to the Galasa project
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package dev.galasa.inttests.simbank.local;
 
 import dev.galasa.Tags;
